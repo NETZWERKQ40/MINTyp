81878976978e875f2ff92195b5933981bec8b742
NETZWERK Q 4.0 NRW <herjuergen@bwnrw.de>
2026-04-16 09:34:57 +0200

    Update print statement from 'Hello' to 'Goodbye'

--- script.js
+++ script.js
@@ -1,4 +1,135 @@
-const QUESTION_COUNT = 20;
+const QUESTION_COUNT = 15;
+
+const jobData = {
+  M: [
+    { name: "Mathematisch-technische/r Softwareentwickler/in", url: "https://www.ausbildung.de/berufe/mathematisch-technischer-softwareentwickler/" },
+    { name: "Kaufmann/-frau für Digitalisierungsmanagement", url: "https://www.ausbildung.de/berufe/kaufmann-digitalisierungsmanagement/" },
+    { name: "Data Analyst/in", url: "https://www.ausbildung.de/berufe/data-analyst/" }
+  ],
+  I: [
+    { name: "Fachinformatiker/in", url: "https://www.ausbildung.de/berufe/fachinformatiker/" },
+    { name: "Fachinformatiker/in für Anwendungsentwicklung", url: "https://www.ausbildung.de/berufe/fachinformatiker-anwendungsentwicklung/" },
+    { name: "Fachinformatiker/in für Daten- und Prozessanalyse", url: "https://www.ausbildung.de/berufe/fachinformatiker-daten-prozessanalyse/" },
+    { name: "Fachinformatiker/in für digitale Vernetzung", url: "https://www.ausbildung.de/berufe/fachinformatiker-digitale-vernetzung/" },
+    { name: "Fachinformatiker/in für Systemintegration", url: "https://www.ausbildung.de/berufe/fachinformatiker-systemintegration/" },
+    { name: "IT-Systemelektroniker/in", url: "https://www.ausbildung.de/berufe/it-system-elektroniker/" },
+    { name: "Elektroniker/in", url: "https://www.ausbildung.de/berufe/elektroniker/" },
+    { name: "Elektroniker/in für Informations- und Systemtechnik", url: "https://www.ausbildung.de/berufe/elektroniker-informations-systemtechnik/" },
+    { name: "Technische/r Assistent/in für Informatik", url: "https://www.ausbildung.de/berufe/technischer-assistent-informatik/" },
+    { name: "Informationselektroniker/in", url: "https://www.ausbildung.de/berufe/informationselektroniker/" },
+    { name: "Staatlich geprüfte/r Informatiker/in", url: "https://www.ausbildung.de/berufe/informatiker/" },
+    { name: "Kaufmann/-frau für IT-System-Management", url: "https://www.ausbildung.de/berufe/kaufmann-it-system-management/" },
+    { name: "Beamter/Beamtin Fernmelde- und Elektronische Aufklärung", url: "https://www.ausbildung.de/berufe/beamter-fermelde-und-elektronische-aufklaerung/" },
+    { name: "Elektroniker/in für Automatisierungstechnik", url: "https://www.ausbildung.de/berufe/elektroniker-automatisierungstechnik/" },
+    { name: "Elektroniker/-in für Automatisierungs- und Systemtechnik", url: "https://www.ausbildung.de/berufe/elektroniker-automatisierungs-systemtechnik/" },
+    { name: "Kaufmann/-frau für Digitalisierungsmanagement", url: "https://www.ausbildung.de/berufe/kaufmann-digitalisierungsmanagement/" },
+    { name: "Mediengestalter/in Digital und Print", url: "https://www.ausbildung.de/berufe/mediengestalter-digital-print/" },
+    { name: "Produktionstechnologe/-technologin", url: "https://www.ausbildung.de/berufe/produktionstechnologe/" },
+    { name: "Programmierer/in", url: "https://www.ausbildung.de/berufe/programmierer/" },
+    { name: "Softwareentwickler/in", url: "https://www.ausbildung.de/berufe/softwareentwickler/" }
+  ],
+  N: [
+    { name: "Chemielaborant/in", url: "https://www.ausbildung.de/berufe/chemielaborant/" },
+    { name: "Chemikant/in", url: "https://www.ausbildung.de/berufe/chemikant/" },
+    { name: "Biologielaborant/in", url: "https://www.ausbildung.de/berufe/biologielaborant/" },
+    { name: "Lacklaborant/in", url: "https://www.ausbildung.de/berufe/lacklaborant/" },
+    { name: "Pharmakant/in", url: "https://www.ausbildung.de/berufe/pharmakant/" },
+    { name: "Pflanzentechnologe/-technologin", url: "https://www.ausbildung.de/berufe/pflanzentechnologe/" },
+    { name: "Textillaborant/in", url: "https://www.ausbildung.de/berufe/textillaborant/" },
+    { name: "Physiklaborant/in", url: "https://www.ausbildung.de/berufe/physiklaborant/" },
+    { name: "Chemisch-technischer Assistent (CTA)", url: "https://www.ausbildung.de/berufe/chemisch-technischer-assistent/" },
+    { name: "Umwelttechnologe/Umwelttechnologin", url: "https://www.ausbildung.de/berufe/umwelttechnologe/" },
+    { name: "Umweltschutztechnische/r Assistent/in", url: "https://www.ausbildung.de/berufe/umweltschutztechnischer-assistent/" },
+    { name: "MTR (Medizinische/r Technologe/Technologin für Radiologie)", url: "https://www.ausbildung.de/berufe/medizinischer-technologe-radiologie/" },
+    { name: "MTF (Medizinische/r Technologe/Technologin für Funktionsdiagnostik)", url: "https://www.ausbildung.de/berufe/medizinischer-technologe-funktionsdiagnostik/" },
+    { name: "Anästhesietechnische/r Assistent/in (ATA)", url: "https://www.ausbildung.de/berufe/anaesthesietechnischer-assistent/" },
+    { name: "Agrartechnische/r Assistent/in", url: "https://www.ausbildung.de/berufe/agrartechnischer-assistent/" },
+    { name: "Chemielaborjungwerker/in", url: "https://www.ausbildung.de/berufe/chemielaborjungwerker/" },
+    { name: "MTL (Medizinische/r Technologe/Technologin für Laboratoriumsanalytik)", url: "https://www.ausbildung.de/berufe/medizinischer-technologe-laboratoriumsanalytik/" },
+    { name: "Pharmazeutisch-technische/r Assistent/in (PTA)", url: "https://www.ausbildung.de/berufe/pharmazeutisch-technischer-assistent/" },
+    { name: "Produktionsfachkraft Chemie", url: "https://www.ausbildung.de/berufe/produktionsfachkraft-chemie/" },
+    { name: "Werkstoffprüfer/in", url: "https://www.ausbildung.de/berufe/werkstoffpruefer/" },
+    { name: "Papiertechnologe/-technologin", url: "https://www.ausbildung.de/berufe/papiertechnologe/" }
+  ],
+  T: [
+    { name: "Kfz-Mechatroniker/in", url: "https://www.ausbildung.de/berufe/kfz-mechatroniker/" },
+    { name: "Mechatroniker/in", url: "https://www.ausbildung.de/berufe/mechatroniker/" },
+    { name: "Elektroniker/in", url: "https://www.ausbildung.de/berufe/elektroniker/" },
+    { name: "Fluggerätmechaniker/in", url: "https://www.ausbildung.de/berufe/fluggeraetmechaniker/" },
+    { name: "Technische/r Produktdesigner/in", url: "https://www.ausbildung.de/berufe/technischer-produktdesigner/" },
+    { name: "Metallbauer/in", url: "https://www.ausbildung.de/berufe/metallbauer/" },
+    { name: "Zerspanungsmechaniker/in", url: "https://www.ausbildung.de/berufe/zerspanungsmechaniker/" },
+    { name: "Industriemechaniker/in", url: "https://www.ausbildung.de/berufe/industriemechaniker/" },
+    { name: "Werkzeugmechaniker/in", url: "https://www.ausbildung.de/berufe/werkzeugmechaniker/" },
+    { name: "Schiffsmechaniker/in", url: "https://www.ausbildung.de/berufe/schiffsmechaniker/" },
+    { name: "Verfahrensmechaniker/in", url: "https://www.ausbildung.de/berufe/verfahrensmechaniker/" },
+    { name: "Maschinen- und Anlagenführer/in", url: "https://www.ausbildung.de/berufe/maschinen-anlagenfuehrer/" },
+    { name: "Feinwerkmechaniker/in", url: "https://www.ausbildung.de/berufe/feinwerkmechaniker/" },
+    { name: "Anlagenmechaniker/in", url: "https://www.ausbildung.de/berufe/anlagenmechaniker/" },
+    { name: "Oberflächenbeschichter/in", url: "https://www.ausbildung.de/berufe/oberflaechenbeschichter/" },
+    { name: "Fluggerätelektroniker/in", url: "https://www.ausbildung.de/berufe/fluggeraetelektroniker/" },
+    { name: "Fertigungsmechaniker/in", url: "https://www.ausbildung.de/berufe/fertigungsmechaniker/" },
+    { name: "Baustoffprüfer/in", url: "https://www.ausbildung.de/berufe/baustoffpruefer/" },
+    { name: "Konstruktionsmechaniker/in", url: "https://www.ausbildung.de/berufe/konstruktionsmechaniker/" },
+    { name: "Gießereimechaniker/in", url: "https://www.ausbildung.de/berufe/giessereimechaniker/" },
+    { name: "Elektroniker/in Geräte und Systeme", url: "https://www.ausbildung.de/berufe/elektroniker-geraete-systeme/" },
+    { name: "Holzmechaniker/in", url: "https://www.ausbildung.de/berufe/holzmechaniker/" },
+    { name: "Süßwarentechnologe/-technologin", url: "https://www.ausbildung.de/berufe/suesswarentechnologe/" },
+    { name: "Elektroanlagenmonteur/in", url: "https://www.ausbildung.de/berufe/elektroanlagenmonteur/" },
+    { name: "Elektroniker/in für Automatisierungstechnik", url: "https://www.ausbildung.de/berufe/elektroniker-automatisierungstechnik/" },
+    { name: "Mikrotechnologe/-technologin", url: "https://www.ausbildung.de/berufe/mikrotechnologe/" },
+    { name: "Industriemechaniker/in Maschinen- und Anlagenbau", url: "https://www.ausbildung.de/berufe/industriemechaniker-maschinen-anlagenbau/" },
+    { name: "Zerspanungsmechaniker/in für Drehmaschinensysteme", url: "https://www.ausbildung.de/berufe/zerspanungsmechaniker-drehmaschinensysteme/" },
+    { name: "Zerspanungsmechaniker/in in Frästechnik", url: "https://www.ausbildung.de/berufe/zerspanungsmechaniker-fraestechnik/" },
+    { name: "Elektroniker/in für Energie- und Gebäudetechnik", url: "https://www.ausbildung.de/berufe/elektroniker-energie-gebaeudetechnik/" },
+    { name: "Elektroniker/in für Maschinen und Antriebstechnik", url: "https://www.ausbildung.de/berufe/elektroniker-maschinen-antriebstechnik/" },
+    { name: "Präzisionswerkzeugmechaniker/in", url: "https://www.ausbildung.de/berufe/praezisionswerkzeugmechaniker/" },
+    { name: "Industriemechaniker/in Instandhaltung", url: "https://www.ausbildung.de/berufe/industriemechaniker-instandhaltung/" },
+    { name: "Verfahrenstechnologe/-technologin Metall", url: "https://www.ausbildung.de/berufe/verfahrenstechnologe-metall/" },
+    { name: "Elektroniker/in für Gebäudesystemintegration", url: "https://www.ausbildung.de/berufe/elektroniker-gebaeudesystemintegration/" },
+    { name: "Mechaniker/in für Reifen- und Vulkanisationstechnik", url: "https://www.ausbildung.de/berufe/mechaniker-reifen-vulkanisationstechnik/" },
+    { name: "Produktionstechnologe/-technologin", url: "https://www.ausbildung.de/berufe/produktionstechnologe/" },
+    { name: "Fachkraft für Rohr-, Kanal- und Industrieservice", url: "https://www.ausbildung.de/berufe/fachkraft-rohr-kanal-industrieservice/" },
+    { name: "Fachkraft für Lebensmitteltechnik", url: "https://www.ausbildung.de/berufe/fachkraft-lebensmitteltechnik/" },
+    { name: "Produktionsfachkraft Chemie", url: "https://www.ausbildung.de/berufe/produktionsfachkraft-chemie/" },
+    { name: "Bergbautechnologe/-technologin", url: "https://www.ausbildung.de/berufe/bergbautechnologe/" },
+    { name: "Industrieelektriker/in", url: "https://www.ausbildung.de/berufe/industrieelektriker/" },
+    { name: "Aufbereitungsmechaniker/in", url: "https://www.ausbildung.de/berufe/aufbereitungsmechaniker/" },
+    { name: "Verfahrensmechaniker/in für Beschichtungstechnik", url: "https://www.ausbildung.de/berufe/verfahrensmechaniker-beschichtungstechnik/" },
+    { name: "Anlagenmechaniker:in für Sanitär-, Heizungs- und Klimatechnik", url: "https://www.ausbildung.de/berufe/anlagenmechaniker-shk/" },
+    { name: "Industriemechaniker/in Produktionstechnik", url: "https://www.ausbildung.de/berufe/industriemechaniker-produktionstechnik/" },
+    { name: "Industrietechnologe/-technologin", url: "https://www.ausbildung.de/berufe/industrietechnologe/" },
+    { name: "Medientechnologe/-technologin Druck", url: "https://www.ausbildung.de/berufe/medientechnologe-druck/" },
+    { name: "Medientechnologe/-technologin Druckverarbeitung", url: "https://www.ausbildung.de/berufe/medientechnologe-druckverarbeitung/" },
+    { name: "Medientechnologe/-technologin", url: "https://www.ausbildung.de/berufe/medientechnologe/" },
+    { name: "Medientechnologe/-technologin Siebdruck", url: "https://www.ausbildung.de/berufe/medientechnologe-siebdruck/" },
+    { name: "Rollladen- und Sonnenschutzmechatroniker/in", url: "https://www.ausbildung.de/berufe/rollladen-sonnenschutzmechatroniker/" },
+    { name: "Chirurgiemechaniker/in", url: "https://www.ausbildung.de/berufe/chirurgiemechaniker/" },
+    { name: "Verfahrensmechaniker/in für Brillenoptik", url: "https://www.ausbildung.de/berufe/verfahrensmechaniker-brillenoptik/" },
+    { name: "Elektroniker/in für Gebäude- und Infrastruktursysteme", url: "https://www.ausbildung.de/berufe/elektroniker-gebaeude-infrastruktursysteme/" },
+    { name: "Fachkraft für Wasserversorgungstechnik", url: "https://www.ausbildung.de/berufe/fachkraft-wasserversorgungstechnik/" },
+    { name: "Karosserie- und Fahrzeugbaumechaniker/in", url: "https://www.ausbildung.de/berufe/karosserie-fahrzeugbaumechaniker/" },
+    { name: "Fachkraft für Abwassertechnik", url: "https://www.ausbildung.de/berufe/fachkraft-abwassertechnik/" },
+    { name: "Biologisch-Technische/r Assistent/in (BTA)", url: "https://www.ausbildung.de/berufe/biologisch-technischer-assistent/" },
+    { name: "Informationstechnische/r Assistent/in", url: "https://www.ausbildung.de/berufe/informationstechnischer-assistent/" },
+    { name: "Elektrotechniker/in", url: "https://www.ausbildung.de/berufe/elektrotechniker/" },
+    { name: "Geomatiker/in", url: "https://www.ausbildung.de/berufe/geomatiker/" },
+    { name: "Kraftfahrzeugtechniker/in", url: "https://www.ausbildung.de/berufe/kraftfahrzeugtechniker/" },
+    { name: "Augenoptiker/in", url: "https://www.ausbildung.de/berufe/augenoptiker/" },
+    { name: "Feinoptiker/in", url: "https://www.ausbildung.de/berufe/feinoptiker/" },
+    { name: "Hörakustiker/in", url: "https://www.ausbildung.de/berufe/hoerakustiker/" },
+    { name: "Vermessungstechniker/in", url: "https://www.ausbildung.de/berufe/vermessungstechniker/" },
+    { name: "Werkstoffprüfer/in", url: "https://www.ausbildung.de/berufe/werkstoffpruefer/" },
+    { name: "Technische/r Zeichner/in", url: "https://www.ausbildung.de/berufe/technischer-zeichner/" }
+  ]
+};
+
+const iconPools = {
+  M: ["∑", "📊", "📈", "🧮"],
+  I: ["💻", "</>", "🖥️", "🗄️", "🔐"],
+  N: ["🔬", "🧪", "⚗️", "🌿", "🧬"],
+  T: ["⚙️", "🔧", "🛠️", "📐", "🏗️"]
+};
 
 const questionPool = [
   {
@@ -303,6 +434,7 @@ const currentNumber = document.getElementById("current-number");
 const totalNumber = document.getElementById("total-number");
 const progressLabel = document.getElementById("progress-label");
 const progressFill = document.getElementById("progress-fill");
+const resultSections = document.getElementById("result-sections");
 
 function showScreen(screen) {
   startScreen.classList.remove("active");
@@ -387,7 +519,6 @@ function getScores() {
 
 function getPercentages(scores) {
   const total = quizQuestions.length;
-
   return {
     M: Math.round((scores.M / total) * 100),
     I: Math.round((scores.I / total) * 100),
@@ -410,19 +541,119 @@ function buildResultText(percentages) {
   return resultTexts[topType];
 }
 
+function getTopTypes(percentages) {
+  const maxValue = Math.max(percentages.M, percentages.I, percentages.N, percentages.T);
+  return Object.keys(percentages).filter((key) => percentages[key] === maxValue);
+}
+
+function getTypeLabel(type) {
+  return {
+    M: "Mathematik",
+    I: "Informatik",
+    N: "Naturwissenschaft",
+    T: "Technik"
+  }[type];
+}
+
+function getIconForJob(type, index) {
+  const pool = iconPools[type];
+  return pool[index % pool.length];
+}
+
+function createJobsGrid(type) {
+  const jobs = jobData[type] || [];
+  const grid = document.createElement("div");
+  grid.className = "jobs-grid";
+
+  jobs.forEach((job, index) => {
+    const link = document.createElement("a");
+    link.className = "job-card";
+    link.href = job.url;
+    link.target = "_blank";
+    link.rel = "noopener noreferrer";
+    link.innerHTML = `
+      <span class="job-icon">${getIconForJob(type, index)}</span>
+      <span class="job-text">${job.name}</span>
+      <span class="job-arrow">→</span>
+    `;
+    grid.appendChild(link);
+  });
+
+  return grid;
+}
+
+function createResultSection(type, percentage, isTop) {
+  const section = document.createElement("div");
+  section.className = `result-section${isTop ? " highlight" : ""}`;
+
+  const head = document.createElement("div");
+  head.className = "result-section-head";
+
+  if (isTop) {
+    const badge = document.createElement("div");
+    badge.className = "result-badge";
+    badge.textContent = "Dein stärkster Bereich";
+    head.appendChild(badge);
+  }
+
+  const titleRow = document.createElement("div");
+  titleRow.className = "result-head";
+  titleRow.innerHTML = `
+    <span>${getTypeLabel(type)}</span>
+    <span>${percentage}%</span>
+  `;
+
+  const bar = document.createElement("div");
+  bar.className = "result-bar";
+
+  const fill = document.createElement("div");
+  fill.className = "result-fill";
+  fill.style.width = `${percentage}%`;
+  bar.appendChild(fill);
+
+  head.appendChild(titleRow);
+  head.appendChild(bar);
+
+  const toggle = document.createElement("button");
+  toggle.type = "button";
+  toggle.className = "dropdown-toggle";
+  toggle.innerHTML = `
+    <span>Ausbildungsberufe anzeigen</span>
+    <span class="dropdown-arrow">⌄</span>
+  `;
+
+  const jobsWrap = document.createElement("div");
+  jobsWrap.className = "jobs-wrap";
+  jobsWrap.appendChild(createJobsGrid(type));
+
+  toggle.addEventListener("click", () => {
+    const isOpen = jobsWrap.classList.contains("open");
+    jobsWrap.classList.toggle("open", !isOpen);
+    toggle.classList.toggle("open", !isOpen);
+  });
+
+  section.appendChild(head);
+  section.appendChild(toggle);
+  section.appendChild(jobsWrap);
+
+  return section;
+}
+
+function renderResultSections(percentages) {
+  resultSections.innerHTML = "";
+  const topTypes = getTopTypes(percentages);
+
+  ["M", "I", "N", "T"].forEach((type) => {
+    const section = createResultSection(type, percentages[type], topTypes.includes(type));
+    resultSections.appendChild(section);
+  });
+}
+
 function showResults() {
   const scores = getScores();
   const percentages = getPercentages(scores);
 
-  document.getElementById("percent-m").textContent = `${percentages.M}%`;
-  document.getElementById("percent-i").textContent = `${percentages.I}%`;
-  document.getElementById("percent-n").textContent = `${percentages.N}%`;
-  document.getElementById("percent-t").textContent = `${percentages.T}%`;
-
-  document.getElementById("bar-m").style.width = `${percentages.M}%`;
-  document.getElementById("bar-i").style.width = `${percentages.I}%`;
-  document.getElementById("bar-n").style.width = `${percentages.N}%`;
-  document.getElementById("bar-t").style.width = `${percentages.T}%`;
+  renderResultSections(percentages);
 
   document.getElementById("result-text").textContent = buildResultText(percentages);
   document.getElementById("result-summary").textContent =
